6e538955cc27e5af7b535411dbe1188dbcf0e028
alixtak <amuller.tools@gmail.com>
2023-09-19 05:06:34 +0200

    Updated project with new features

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -49,6 +49,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                    onClick={() => onOpen("members", { server })}
                     className="px-3 py-2 text-sm cursor-pointer">
                         Manage Members
                         <Users className="h-4 w-4 ml-auto" />
@@ -56,6 +57,7 @@ export const ServerHeader = ({
                 )}
                 {isModerator && (
                     <DropdownMenuItem
+                    onClick={() => onOpen("createChannel")}
                     className="px-3 py-2 text-sm cursor-pointer">
                         Create Channel
                         <PlusCircle className="h-4 w-4 ml-auto" />
@@ -66,6 +68,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                    onClick={() => onOpen("deleteServer", { server })}
                     className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Delete Server
                         <Trash className="h-4 w-4 ml-auto" />
@@ -73,6 +76,7 @@ export const ServerHeader = ({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                    onClick={() => onOpen("leaveServer", { server })}
                     className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto" />
